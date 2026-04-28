2d6c9f59490eade532ffcb17cc9f375ee1eb507b
sylabtechnologies <teslacloud@icloud.com>
2026-04-28 12:41:02 -0400

    Leetcode #499 04-25-2026

---
+++ leetcode/Lcode499A.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+// find valid elements
+// https://leetcode.com/problems/valid-elements-in-an-array/description/
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findValidElements(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 1) return {};
+
+        int maxSoFar, minSoFar;
+        maxSoFar = minSoFar = nums[0];
+
+        // sweep right
+        vector<char> isValid(nums.size(), false); // use char vs bool for speed
+        isValid[0] = true;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > maxSoFar || nums[i] < minSoFar) {
+                isValid[i] = true;
+                if (nums[i] > maxSoFar) maxSoFar = nums[i];
+                if (nums[i] < minSoFar) minSoFar = nums[i];
+            }
+        }
+
+        // sweep left
+        maxSoFar = minSoFar = nums[n-1];
+        isValid[n-1] = true;
+        for (int i = n-2; i >= 0; i--) {
+            if (nums[i] > maxSoFar || nums[i] < minSoFar) {
+                isValid[i] = true;
+                if (nums[i] > maxSoFar) maxSoFar = nums[i];
+                if (nums[i] < minSoFar) minSoFar = nums[i];
+            }
+        }
+
+        vector<int> result;
+        for (int i = 0; i < n; i++) {
+            if (isValid[i]) result.push_back(nums[i]); 
+        }
+
+        return result;
+    }
+};
